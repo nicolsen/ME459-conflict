f4e88bc74eaf377f6e831529f72ad71e8e63ecea
nicmolsen <nicholas.olsen@wisc.edu>
2019-10-17 08:41:20 -0500

    More enthusiastic message

--- something.c
+++ something.c
@@ -3,7 +3,7 @@
 #define N 25
 
 int main(int argc, char* argv[]) {
-    printf("Hello world!\n");
+    printf("Hello world!!!!\n");
 
     for (size_t i = 0; i < N; i++) {
         printf("%zu ", i+2);
