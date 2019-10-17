99660317040a288acdc875e85ed7ec5795add047
nicmolsen <nicholas.olsen@wisc.edu>
2019-10-17 08:34:00 -0500

    Alterations and adds sum

--- something.c
+++ something.c
@@ -3,12 +3,18 @@
 #define N 25
 
 int main(int argc, char* argv[]) {
-    printf("Hello world!\n");
+    printf("Hello World!\n");
 
     for (size_t i = 0; i < N; i++) {
-        printf("%zu ", i);
+        printf("%zu ", i+1);
     }
     printf("\n");
+
+    size_t sum = 0;
+    for (size_t i = 0; i < N; i++) {
+        sum += i;
+    }
+    printf("Sum: %zu\n", sum);
 
     return 0;
 }
