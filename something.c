8a55aa21b94042e41de9fb3edf23d22bee3281ea
nicmolsen <nicholas.olsen@wisc.edu>
2019-10-17 08:37:03 -0500

    Alterations and adds alternating sum

--- something.c
+++ something.c
@@ -6,9 +6,20 @@ int main(int argc, char* argv[]) {
     printf("Hello world!\n");
 
     for (size_t i = 0; i < N; i++) {
-        printf("%zu ", i);
+        printf("%zu ", i+2);
     }
     printf("\n");
+
+    int accumulate = 0;
+    for(int i = 0; i < N; i++) {
+        if (i % 2) {
+            accumulate += i;
+        } else {
+            accumulate -= i;
+        }
+    }
+
+    printf("Accumulate: %d\n", accumulate);
 
     return 0;
 }
